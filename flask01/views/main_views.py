e834f63c622c3a42c50b08957de325c3eb72b594
jimin <nijo5093@gmail.com>
2025-03-26 16:24:58 +0900

    flaskv1

--- flask01/views/main_views.py
+++ flask01/views/main_views.py
@@ -10,3 +10,25 @@ def hello():
     return f'{__name__} hello'
 
 # Flask에서 값을 주소줄로 입력받아서 사용하는 방법
+# <변수명> /변수명
+@mbp.route('/<username>')
+def print_string(username):
+    return f'{__name__} {username} hello'
+
+# <자료형:변수명>
+# <path:변수명>: /를 포함한 서브경로 전달
+# <float: 변수명>: float 전달
+# <int: 변수명>: int 전달
+@mbp.route('/path/<path:subpath>')
+def print_path(subpath):
+    return f'{__name__} {subpath} hello'
+
+# 아무런 값을 저장하지 않았을 때 디폴트값인 기본값이 출력된다
+# http://localhost:5001/main/상품명/사과 -> 에러
+# http://localhost:5001/main/items/사과 -> 가능
+@mbp.route('/상품명/')
+@mbp.route('/items/')
+@mbp.route('/items/<itemname>')
+@mbp.route('/items/<itemname>/<quantity>')
+def print_itemname(itemname='기본값', quantity=0):
+    return f'{__name__} {itemname, quantity} hello'
