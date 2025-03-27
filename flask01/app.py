828f4c44f67bc086043ced0a23183af122a5f9ba
jimin <nijo5093@gmail.com>
2025-03-27 16:50:35 +0900

    0327_템플릿을 표준 HTML 구조로 바꾸기 전

--- flask01/app.py
+++ flask01/app.py
@@ -1,14 +1,25 @@
 # flask run --debug --port 5001
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 def create_app():
     
     # 입구 파일을 하나 만들어줍니다.
     # Flask 어플리케이션을 생성하는 코드
     # 이 파일에 다른 파일 경유 없이 main.py를 통해 실행된다면 __name__ 변수에는 'main'라는 문자열이 담김
     app = Flask(__name__)
+    app.config.from_object(config)
 
-    from views import main_views, board_views
+    # ORM을 적용
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from board.views import main_views, board_views
     app.register_blueprint(main_views.mbp)
     app.register_blueprint(board_views.cbp)
 
---
+++ flask01/board/views/main_views.py
@@ -0,0 +1,44 @@
+from flask import Blueprint
+from flask import render_template
+
+# 특정 /url/ 하위에 있는 함수들을 일괄적으로 관리하기 위한 속성 
+# Blueprint(코드에서 부르는 상대적이름, 실제파일명, url에 매칭되는 경로)
+mbp = Blueprint('main', __name__, url_prefix='/')
+
+# localhost:5001/main/
+# localhost:5001로 접속하면 index.html을 화면에 렌더링하도록 변경해주세요.
+@mbp.route('/')
+def index():
+    return render_template('index.html')
+
+# mbp = Blueprint('main', __name__, url_prefix='/main')
+
+# # localhost:5001/main/
+# @mbp.route('/')
+# def hello():
+#     return f'{__name__} hello'
+
+# # Flask에서 값을 주소줄로 입력받아서 사용하는 방법
+# # <변수명> /변수명
+# @mbp.route('/<username>')
+# def print_string(username):
+#     return f'{__name__} {username} hello'
+ 
+# # <자료형:변수명>
+# # <path:변수명>: /를 포함한 서브경로 전달
+# # <float: 변수명>: float 전달
+# # <int: 변수명>: int 전달
+# @mbp.route('/path/<path:subpath>')
+# def print_path(subpath):
+#     return f'{__name__} {subpath} hello'
+
+# # 아무런 값을 저장하지 않았을 때 디폴트값인 기본값이 출력된다
+# # http://localhost:5001/main/상품명/사과 -> 에러
+# # http://localhost:5001/main/items/사과 -> 가능
+# @mbp.route('/상품명/')
+# @mbp.route('/items/')
+# @mbp.route('/items/<itemname>')
+# @mbp.route('/items/<itemname>/<float:quantity>')
+# def print_itemname(itemname='기본값', quantity=0):
+#     print(type(quantity))
+#     return f'{__name__} {itemname, quantity} hello'
